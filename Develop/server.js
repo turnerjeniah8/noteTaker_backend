a2ce5794d32866d07ab66949d9072a1ba5f50e3e
Cyberdev <turnerjeniah8@gmail.com>
2023-01-26 02:02:24 -0800

    Updated the get route. Also completed a create note route, and a delete route for each note

--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 console.log('read');
 // GET Route for notes
 //we want to get and read the data within the notes html
-app.get('/notes', (req, res) =>
+app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
@@ -25,6 +25,61 @@ app.get('*', (req, res) =>
     //we want to send this information to the public index html page.
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+//should reload the notes and keep them updated
+app.route("api/notes")
+    .get(function(req, res) {
+        res.json(database);
+})
+
+.post(function (req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    let newNote = req.body;
+
+    //only want 99 to be the max amount of notes the user can put
+    let idLimit = 99;
+
+    //this will give the max note the same id, that way the user wouldnt be able to create more, and the limit will work
+    for (let i = 0; i < database.length; i++) {
+        let individualNote = database[i];
+        if (individualNote > idLimit) {
+            idLimit = individualNote.id;
+        }
+    }
+    //this will give each note an id number
+newNote.id = idLimit + 1;
+    database.push(newNote)
+    //this will update the database with the note.
+    //this will update the json file and write the information the user types to it
+    fs.writeFile(jsonFilePath, JSON.stringify(database), function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Your note has been Successfully Saved!");
+    });
+    res.json(newNote);
+});
+
+// delete function to delete any note
+//then the db.json file needs to be rewritten after a note has been deleted
+app.delete("/api/notes/:id", function (req, res) {
+    let jsonFilePath = path.join (__dirname, "/db/db.json");
+    for (let i = 0; i < database.length; i++) {
+        if (database[i].id == req.params.id) {
+            database.splice(i, 1);
+        break;
+
+    }
+}
+    fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log(" Your note has been successfully deleted! ");
+        }
+    });
+    res.json(database);
+});
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
