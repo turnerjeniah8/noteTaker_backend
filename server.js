c3b9d390cd7e07c9f5b85eeda29562e4ae2268cf
Cyberdev <turnerjeniah8@gmail.com>
2023-01-26 23:25:57 -0800

    Broke heroku, trying to retrace steps

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 //const api = require();
 const fs = require("fs");
-const notesDB = require("./db/db.json");
-const { randomUUID } = require('uuid');
+const database = require("./db/db.json");
+const { randomUUID } = require('crypto');
 
 const PORT = process.env.PORT || 3001;
 
@@ -34,7 +34,7 @@ app.get("/", (req, res) =>
 app.route("/notes")
     .get(function (req, res) {
         res.json(database);
-});
+})
 
 //I want to give the id a random ID
 //then I want to assign that into a newNote variable
@@ -43,22 +43,22 @@ app.post("api/notes", function (req, res) {
   req.body.id = randomUUID();
   const newNote = req.body;
 
-  notesDB.push(newNote);
+  database.push(newNote);
 
-  fs.writeFileSync("./db/db.json", JSON.stringify(notesDB));
-  res.json(notesDB);
+  fs.writeFileSync("./db/db.json", JSON.stringify(database));
+  res.json(database);
 
 })
 
 // delete function to delete any note
 //then the db.json file needs to be rewritten after a note has been deleted
-//going to search for the id then filter through the previous notes. The note that matched will be deleted
+//going to search for the id then filter through the previous notes. The note that matched willdelete
 app.delete("/api/notes/:id", function (req, res) {
   const id = req.params.id;
 
-  notesDB = notesDB.filter(notes => notes.id !=id);
-  fs.writeFileSync("./db/db.json", JSON.stringify(notesDB));
-  res.json(notesDB);
+  database = database.filter(notes => notes.id !=id);
+  fs.writeFileSync("./db/db.json", JSON.stringify(database));
+  res.json(database);
 })
 
 
