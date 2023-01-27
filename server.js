192d2fc677684ed3276b77db9ce89893baff1d37
Cyberdev <turnerjeniah8@gmail.com>
2023-01-26 23:40:54 -0800

    fixing heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get("api/notes", (req, res) => {res.json(database)});
+app.get("api/notes", (req, res) => { res.json(notesDB)});
 
 app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html')) 
