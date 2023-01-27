e3c4e96bda04da734e535d6f9f5ef4a62daa4f5f
Cyberdev <turnerjeniah8@gmail.com>
2023-01-26 21:55:15 -0800

    Modified server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-const api = require();
+//const api = require();
+const fs = require("fs");
+const database = require("./db/db.json")
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,25 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //we want the application to use the index.js file
-app.use('/api', api);
+//app.use('/api', api);
 
 app.use(express.static('public'));
 console.log('read');
 // GET Route for notes
 //we want to get and read the data within the notes html
-app.get('/', (req, res) =>
+app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET Route for note page
-app.get('*', (req, res) =>
-    //we want to send this information to the public index html page.
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+
 
 //should reload the notes and keep them updated
 app.route("api/notes")
-    .get(function(req, res) {
+    .get(function (req, res) {
         res.json(database);
 })
 
@@ -79,6 +77,12 @@ app.delete("/api/notes/:id", function (req, res) {
         }
     });
     res.json(database);
+});
+
+// GET Route for note page
+app.get('*', function (req, res) {
+    //we want to send this information to the public index html page.
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.listen(PORT, () =>
